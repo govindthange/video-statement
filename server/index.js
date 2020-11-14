ddfe4c1ceabbd21f845c3a8fd1c9d4c62a13d42c
Govind Thange <gthange@yahoo.com>
2020-11-14 12:41:08 +0530

    Modularize code with functions before adding more features like audio/TTS

--- server/index.js
+++ server/index.js
@@ -50,75 +50,6 @@ app.post("/generate", (req, res) => {
         console.log("File Uploaded successfully");
     });
 
-    let overlayImageFilter1 = {
-        filter: 'overlay',
-        inputs: 'rescaled',
-        outputs: 'output1',
-        options: {
-            x: 200,
-            y: 200,
-            enable: 'between(t,2,3)'
-        }
-    };
-
-    let textFilter1 = {
-        filter: 'drawtext',
-        inputs: 'output1',
-        outputs: 'output2',
-        options: {
-            fontfile: 'Lucida Grande.ttf',
-            text: clientName,
-            fontsize: 68,
-            fontcolor: "22128a",
-            alpha: "if(lt(t, 1),0,if(lt(t,2),(t- 1)/1 ,if(lt(t,8),1,if(lt(t,9),(1-(t-8))/1,0))))",
-            x: 10,
-            y: 10
-        }
-    };
-
-    let textFilter2 = {
-        filter: 'drawtext',
-        inputs: 'output2',
-        outputs: 'output3',
-        options: {
-            fontfile: 'Lucida Grande.ttf',
-            text: `Total Investment = INR ${investedValue}`,
-            fontsize: 38,
-            alpha: "if(lt(t,1),0,if(lt(t,2),(t-1)/1,if(lt(t,4),1,if(lt(t,5),(1-(t-4))/1,0))))",
-            x: 10,
-            y: 80
-        }
-    };
-
-    let textFilter3 = {
-        filter: 'drawtext',
-        inputs: 'output3',
-        outputs: 'output4',
-        options: {
-            fontfile: 'Lucida Grande.ttf',
-            text: `Current Value = INR ${currentValue}`,
-            fontsize: 38,
-            alpha: "if(lt(t,1),0,if(lt(t,2),(t-1)/1,if(lt(t,4),1,if(lt(t,5),(1-(t-4))/1,0))))",
-            x: 10,
-            y: 160
-        }
-    };
-
-    let textFilter4 = {
-        filter: 'drawtext',
-        inputs: 'output4',
-        outputs: 'output5',
-        options: {
-            fontfile: 'Lucida Grande.ttf',
-            text: thankYouNote,
-            fontsize: 28,
-            fontcolor: "22128a",
-            alpha: "if(lt(t, 11),0,if(lt(t,12),(t- 1)/1 ,if(lt(t,24),1,if(lt(t,25),(1-(t-24))/1,0))))",
-            x: 100,
-            y: 160
-        }
-    };
-
     ffmpeg("uploads/" + file.name)
         .withOutputFormat(to)
         .input('uploads/sample-logo.png')
@@ -129,18 +60,19 @@ app.post("/generate", (req, res) => {
         ])
         .complexFilter([
             'scale=640:480[rescaled]',
-            overlayImageFilter1,
-            textFilter1,
-            textFilter2,
-            textFilter3,
-            textFilter4
+            getOverlayFilter(2, 3, 200, 200, 'rescaled', 'output1'),
+            getTextFilter(clientName, 68, 1, 6, 10, 10, 'output1', 'output2'),
+            getTextFilter(`Total Investment = INR ${investedValue}`, 38, 1, 2, 10, 80, 'output2', 'output3'),
+            getTextFilter(`Current Value = INR ${currentValue}`, 38, 1, 2, 10, 160, 'output3', 'output4'),
+            getTextFilter(thankYouNote, 28, 11, 12, 100, 160, 'output4', 'output5')
         ], 'output5')
         .audioCodec('aac')
         .on('start', function(commandLine) {
-            console.log('Spawned Ffmpeg with command: ' + commandLine);
+            console.log('Applying text and image filters...');
+            //console.log('Command line: ' + commandLine);
         })
         .on("end", function(stdout, stderr) {
-            console.log("Finished");
+            console.log("Finished adding text and image filters.");
             res.download(__dirname + fileName, function(err) {
                 if (err) throw err;
                 /*
@@ -166,3 +98,41 @@ app.post("/generate", (req, res) => {
         })
         .saveToFile(__dirname + fileName);
 });
+
+function getOverlayFilter(startTime, duration, x, y, ip, op) {
+    return {
+        filter: 'overlay',
+        inputs: ip,
+        outputs: op,
+        options: {
+            x: x,
+            y: y,
+            enable: `between(t,${startTime},${duration})`
+        }
+    };
+}
+
+function getTextFilter(txt, font, startTime, duration, x, y, ip, op) {
+    return {
+        filter: 'drawtext',
+        inputs: ip,
+        outputs: op,
+        options: {
+            fontfile: 'Lucida Grande.ttf',
+            text: txt,
+            fontsize: font,
+            fontcolor: "22128a",
+            alpha: getAlpha(startTime, 1, duration, 1),
+            x: x,
+            y: y
+        }
+    };
+}
+
+function getAlpha(fadeInStartTime, fadeInLength, opaqueDuration, fadeOutLength) {
+    let t1 = fadeInStartTime;
+    let t2 = fadeInLength;
+    let t3 = opaqueDuration;
+    let t4 = fadeOutLength;
+    return `if(lt(t,${t1}),0,if(lt(t,${t1+t2}),(t-${t1})/${t2},if(lt(t,${t1+t2+t3}),1,if(lt(t,${t1+t2+t3+t4}),(${t4}-(t${-t1-t2-t3}))/${t4},0))))`;
+}
