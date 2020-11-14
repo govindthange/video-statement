0e712d1232929a231ed3957f9b9c3b46f3491fb9
Govind Thange <gthange@yahoo.com>
2020-11-14 10:22:21 +0530

    Add draw-text filter ontop of overlay-image filter

--- server/index.js
+++ server/index.js
@@ -27,21 +27,20 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 app.listen(5000, () => {
-    console.log("Video Report Server is listening on Port 5000")
+    console.log("Video Report Server is listening on Port 5000");
+    console.log('DIR = ' + __dirname);
 })
 
 // Ref: https://github.com/fluent-ffmpeg/node-fluent-ffmpeg
 app.post("/generate", (req, res) => {
+
     let clientName = req.body.clientName;
     let currentValue = req.body.currentValue;
     let investedValue = req.body.investedValue;
     let thankYouNote = req.body.thankYouNote;
-
     let to = req.body.to;
     let file = req.files.file;
     let fileName = `output.${to}`;
-    console.log(to);
-    console.log(file);
 
     file.mv("uploads/" + file.name, function(err) {
         if (err) {
@@ -51,50 +50,94 @@ app.post("/generate", (req, res) => {
         console.log("File Uploaded successfully");
     });
 
+    let overlayImageFilter1 = {
+        filter: 'overlay',
+        inputs: 'rescaled',
+        outputs: 'output1',
+        options: {
+            x: 200,
+            y: 200,
+            enable: 'between(t,2,3)'
+        }
+    };
+
+    let textFilter1 = {
+        filter: 'drawtext',
+        inputs: 'output1',
+        outputs: 'output2',
+        options: {
+            fontfile: 'Lucida Grande.ttf',
+            text: clientName,
+            fontsize: 68,
+            fontcolor: "22128a",
+            alpha: "if(lt(t, 1),0,if(lt(t,2),(t- 1)/1 ,if(lt(t,8),1,if(lt(t,9),(1-(t-8))/1,0))))",
+            x: 10,
+            y: 10
+        }
+    };
+
+    let textFilter2 = {
+        filter: 'drawtext',
+        inputs: 'output2',
+        outputs: 'output3',
+        options: {
+            fontfile: 'Lucida Grande.ttf',
+            text: `Total Investment = INR ${investedValue}`,
+            fontsize: 38,
+            alpha: "if(lt(t,1),0,if(lt(t,2),(t-1)/1,if(lt(t,4),1,if(lt(t,5),(1-(t-4))/1,0))))",
+            x: 10,
+            y: 80
+        }
+    };
+
+    let textFilter3 = {
+        filter: 'drawtext',
+        inputs: 'output3',
+        outputs: 'output4',
+        options: {
+            fontfile: 'Lucida Grande.ttf',
+            text: `Current Value = INR ${currentValue}`,
+            fontsize: 38,
+            alpha: "if(lt(t,1),0,if(lt(t,2),(t-1)/1,if(lt(t,4),1,if(lt(t,5),(1-(t-4))/1,0))))",
+            x: 10,
+            y: 160
+        }
+    };
+
+    let textFilter4 = {
+        filter: 'drawtext',
+        inputs: 'output4',
+        outputs: 'output5',
+        options: {
+            fontfile: 'Lucida Grande.ttf',
+            text: thankYouNote,
+            fontsize: 28,
+            fontcolor: "22128a",
+            alpha: "if(lt(t, 11),0,if(lt(t,12),(t- 1)/1 ,if(lt(t,24),1,if(lt(t,25),(1-(t-24))/1,0))))",
+            x: 100,
+            y: 160
+        }
+    };
+
     ffmpeg("uploads/" + file.name)
         .withOutputFormat(to)
-        .videoFilter({
-            filter: 'drawtext',
-            options: {
-                fontfile: 'Lucida Grande.ttf',
-                text: clientName,
-                fontsize: 68,
-                fontcolor: "22128a",
-                alpha: "if(lt(t, 1),0,if(lt(t,2),(t- 1)/1 ,if(lt(t,8),1,if(lt(t,9),(1-(t-8))/1,0))))",
-                x: 10,
-                y: 10
-            }
-        }, {
-            filter: 'drawtext',
-            options: {
-                fontfile: 'Lucida Grande.ttf',
-                text: `Total Investment = INR ${investedValue}`,
-                fontsize: 38,
-                alpha: "if(lt(t,1),0,if(lt(t,2),(t-1)/1,if(lt(t,4),1,if(lt(t,5),(1-(t-4))/1,0))))",
-                x: 10,
-                y: 80
-            }
-        }, {
-            filter: 'drawtext',
-            options: {
-                fontfile: 'Lucida Grande.ttf',
-                text: `Current Value = INR ${currentValue}`,
-                fontsize: 38,
-                alpha: "if(lt(t,1),0,if(lt(t,2),(t-1)/1,if(lt(t,4),1,if(lt(t,5),(1-(t-4))/1,0))))",
-                x: 10,
-                y: 160
-            }
-        }, {
-            filter: 'drawtext',
-            options: {
-                fontfile: 'Lucida Grande.ttf',
-                text: thankYouNote,
-                fontsize: 28,
-                fontcolor: "22128a",
-                alpha: "if(lt(t, 11),0,if(lt(t,12),(t- 1)/1 ,if(lt(t,24),1,if(lt(t,25),(1-(t-24))/1,0))))",
-                x: 100,
-                y: 160
-            }
+        .input('uploads/sample-logo.png')
+        .addInput('uploads/hello.mp3')
+        //.videoFilter(textFilter1, textFilter2, textFilter3, textFilter4)
+        .addOption([
+            '-strict -2'
+        ])
+        .complexFilter([
+            'scale=640:480[rescaled]',
+            overlayImageFilter1,
+            textFilter1,
+            textFilter2,
+            textFilter3,
+            textFilter4
+        ], 'output5')
+        .audioCodec('aac')
+        .on('start', function(commandLine) {
+            console.log('Spawned Ffmpeg with command: ' + commandLine);
         })
         .on("end", function(stdout, stderr) {
             console.log("Finished");
